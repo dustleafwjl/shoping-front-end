cb36c99033e0f50214400836a905bcb54686ca75
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-15 23:27:44 +0800

    [jianglin wang] feat: add order page

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,9 +1,61 @@
 import React from "react";
+import { Table, Button } from 'antd';
 
 class Order extends React.Component {
+
+
   render() {
+    const columns = [
+      { title: '名字', dataIndex: 'name', key: 'name', align: "center" },
+      { title: '单价', dataIndex: 'price', key: 'price', align: "center" },
+      { title: '数量', dataIndex: 'count', key: 'count', align: "center" },
+      { title: '单位', dataIndex: 'unit', key: 'unit', align: "center" },
+      {
+        title: '操作',
+        dataIndex: '',
+        key: 'x',
+        align: "center",
+        render: () => <Button style={{
+                        position: "absolute", 
+                        left: "50%", 
+                        top: "50%",
+                        transform: 'translate(-50%, -50%)'}}
+                        danger>Delete</Button>,
+      },
+    ];
+    
+    const data = [
+      {
+        key: 1,
+        name: '可乐',
+        price: 32,
+        count: '12',
+        unit: '元',
+      },
+      {
+        key: 2,
+        name: '可乐',
+        price: 32,
+        count: '12',
+        unit: '元',
+      },
+      {
+        key: 3,
+        name: '可乐',
+        price: 32,
+        count: '12',
+        unit: '元',
+      }
+    ];
     return (
-      <div>Order</div>
+      <div style={{
+        padding: "20px 40px"
+      }}>
+        <Table
+          columns={columns}
+          dataSource={data}
+        />
+      </div>
     );
   }
 }
